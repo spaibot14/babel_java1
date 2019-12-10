6721e3e3918954a0cc7b231f823c3607d3e928ea
jorge.mcabellos <jorge.mcabellos@1cabellosX650G1.babel-si.local>
2019-12-10 12:06:06 +0100

    Commit inicial

---
+++ prueba_babel1/src/prueba_babel1/calcular.java
@@ -0,0 +1,5 @@
+package prueba_babel1;
+
+public class calcular {
+
+}
---
+++ prueba_babel1/src/prueba_babel1/name_sumatorio.java
@@ -0,0 +1,30 @@
+package prueba_babel1;
+
+public class name_sumatorio {
+
+	public static void main(String[] args) {
+		int a = 50;
+		int b = 16;
+	
+		
+		int men = menor(a,b);
+		int may = mayor(a,b);
+		System.out.println("suma de los pares" + suma(men,may));
+		
+	private static int suma(int a, int b) {
+		int suma=0;
+		for (int i = a; i < b; ++i) {
+			if(i%2==0) {
+				suma+=i;
+			}
+	}
+		
+	}
+	
+	private static int menor (int a, int b) {
+		return a>b?b:a;
+	}
+	private static int mayor (int a, int b) {
+		return a>b?a:b;
+	}
+}
